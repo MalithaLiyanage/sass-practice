b2303f3074e014a2b0be359c5cf8499a4cd5bcfc
Malitha Liyanage <malitharanu@gmail.com>
2022-02-16 21:38:03 +0530

    Configured gulpfile to purge unwanted css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
   return src('dawn/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['dawn/**/*.scss'], buildStyles)
+  watch(['dawn/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
